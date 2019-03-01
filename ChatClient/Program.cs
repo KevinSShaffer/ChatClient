58adb7f10e1e74ddaf79cac2f837739d9e1e5b76
Kevin Shaffer <kshaffer@scramsystems.com>
2019-02-28 18:29:30 -0700

    Initial Commit

--- ChatClient/Program.cs
+++ ChatClient/Program.cs
@@ -1,12 +1,34 @@
 ﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
 
 namespace ChatClient
 {
     class Program
     {
+        const int port = 13000;
+        static readonly IPAddress server = IPAddress.Parse("127.0.0.1");
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine($"Connection to server: {server.ToString()}:{port}");
+
+            var client = new TcpClient(new IPEndPoint(server, port));
+
+            Console.WriteLine("Connected!");
+
+            using (var stream = client.GetStream())
+            {
+                while (true)
+                {
+                    string message = Console.ReadLine();
+
+                    stream.Write(Encoding.ASCII.GetBytes(message));
+
+                    Console.WriteLine($"Send: {message}");
+                }
+            }
         }
     }
 }
